8378cabf02977eec8b1a83cf9813c64b68cf4880
brianmcarey <careyb@mindspring.com>
2016-04-21 16:36:25 -0400

    ui work

--- src/main/java/com/careydevelopment/nativeadbrowser/controller/LatestController.java
+++ src/main/java/com/careydevelopment/nativeadbrowser/controller/LatestController.java
@@ -12,12 +12,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.careydevelopment.nativeadbrowser.jpa.entity.NativeAd;
 import com.careydevelopment.nativeadbrowser.jpa.repository.DomainAdRepository;
+import com.careydevelopment.nativeadbrowser.util.Constants;
 
 @Controller
-public class LatestController {
+public class LatestController implements Constants {
 
 	@Autowired
 	DomainAdRepository domainAdRepository;
@@ -25,19 +27,70 @@ public class LatestController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(LatestController.class);
 	
     @RequestMapping("/latest")
-    public String latest(Model model) {    	    
+    public String latest(Model model, @RequestParam(value="pageNum", required=false) String pageNum) {    	    
     	model.addAttribute("homeActive", "active");
+ 
+    	int page = getPage(pageNum);
     	
-    	Pageable topTwelve = new PageRequest(0,12);
-    	Page<NativeAd> ads = domainAdRepository.findNativeAds(topTwelve);
+    	Pageable pageable = new PageRequest(page,RESULTS_PER_PAGE);
+    	Page<NativeAd> ads = domainAdRepository.findNativeAds(pageable);
     	
     	List<NativeAd> nads = new ArrayList<NativeAd>();
     	for (NativeAd ad : ads) {
     		nads.add(ad);
     	}
     	
+    	setPagination(ads,model,page);
+    	
     	model.addAttribute("nativeAds",nads);
     	
+    	
         return "latest";
+    }
+    
+    
+    private void setPagination (Page<NativeAd> ads, Model model, int page) {
+    	model.addAttribute("isFirst", ads.isFirst());
+    	model.addAttribute("isLast", ads.isLast());
+    	
+    	if (!ads.isLast()) {
+    		model.addAttribute("nextPageNum", page+1);
+    	}
+    	
+    	if (!ads.isFirst()) {
+    		model.addAttribute("previousPageNum", page-1);
+    	}	
+    	
+    	model.addAttribute("showingResults",getShowingResults(ads,page));
+    }
+    
+    
+    private String getShowingResults(Page<NativeAd> ads,int page) {
+    	StringBuilder builder = new StringBuilder();
+    	
+    	int start = (page * RESULTS_PER_PAGE) + 1;
+    	int end = start + ads.getSize() - 1;
+    	
+    	builder.append("Showing Results ");    	    	
+    	builder.append(start);
+    	builder.append(" - ");
+    	builder.append(end);
+    	
+    	return builder.toString();
+    }
+    
+    
+    private int getPage(String pageNum) {
+    	int page = 0;
+    	
+    	if (pageNum != null) {
+    		try {
+    			page = new Integer(pageNum);
+    		} catch (NumberFormatException ne) {
+    			LOGGER.warn("Page number isn't a number! " +pageNum);
+    		}
+    	}
+    	
+    	return page;
     }
 }
